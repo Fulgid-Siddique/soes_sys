00ae07d4550bbaba7be65675d6132195bf957c72
Siddique <dba.siddique@gmail.com>
2020-12-19 16:01:30 +0500

    Applicant Model and InMemory Applicant Repository Built

---
+++ SOES_MVC/SOES.Core/Models/Applicant.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOES.Core.Models
+{
+    public class Applicant
+    {
+        public string id { get; set; }
+        [DisplayName("Name")]
+        public string ApplicantName { get; set; }
+        [DisplayName("F/Name")]
+        public string FatherName { get; set; }
+        public string DOB { get; set; }
+        [StringLength(13)]
+        public string CNIC { get; set; }
+        [StringLength(9)]
+        public string PassportNo { get; set; }
+        public string PPIssueDate { get; set; }
+        public string PPExpiryDate { get; set; }
+        public string PPIssuePlace { get; set; }
+        public string Trade { get; set; }
+        public string Address { get; set; }
+        public string Education { get; set; }
+        public string LocalExpereince { get; set; }
+        public string AbrodaExpereince { get; set; }
+        public string CellNo1 { get; set; }
+        public string CellNo2 { get; set; }
+        public string Ref { get; set; }
+        public string Image { get; set; }
+
+        public Applicant()
+        {
+            this.id = Guid.NewGuid().ToString();
+        }
+
+
+    }
+}
---
+++ SOES_MVC/SOES.DataAccess.InMemory/ApplicantRepository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using System.SOES.Core;
+using SOES.Core.Models;
+
+namespace SOES.DataAccess.InMemory
+{
+    class ApplicantRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Applicant> applicants = new List<Applicant>();
+        public ApplicantRepository()
+        {
+            applicants = cache["applicants"] as List<Applicant>;
+            if (applicants == null)
+            {
+                applicants = new List<Applicant>();
+            }
+        }
+        public void Commit()
+        {
+            cache["applicants"] = applicants;
+        }
+
+        public void Insert(Applicant a)
+        {
+            applicants.Add(a);
+        }
+        public void Update(Applicant applicant)
+        {
+            Applicant applicanttoUpdate = applicants.Find(a => a.id == applicant.id);
+            if (applicanttoUpdate != null)
+            {
+                applicanttoUpdate = applicant;
+            }
+            else
+            {
+                throw new Exception("Applicant Not Found");
+            }
+        }
+
+        public Applicant Find(string id)
+        {
+            Applicant applicant = applicants.Find(a => a.id == id);
+            if (applicant != null)
+            {
+                return applicant;
+            }
+            else
+            {
+                throw new Exception("Applicant Not Found");
+
+            }
+        }
+
+        public IQueryable<Applicant> Collection()
+        {
+            return applicants.AsQueryable();
+        }
+
+        public void Delete (string id)
+        {
+            Applicant applicanttoDelete = applicants.Find(a => a.id == id);
+            if (applicanttoDelete != null)
+            {
+                applicants.Remove(applicanttoDelete);
+            }
+            else
+            {
+                throw new Exception("Applicant Not Found");
+            }
+        }
+    }
+}
